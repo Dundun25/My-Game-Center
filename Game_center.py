0351c141d1b0b7071f322022c03ce339675e58d6
Raihan Hanif <75092855+Dundun25@users.noreply.github.com>
2021-03-07 23:37:37 +0700

    Add files via upload

--- Game_center.py
+++ Game_center.py
@@ -14,90 +14,6 @@ class Game:
         self.age = age
         self.gender = gender
 
-    def guessing_game(self):
-        sleep(1)
-        menu = False
-        new_in_selected_game = True
-
-        while True:
-            if menu == False:
-                clear()
-                if new_in_selected_game:
-                    print("Ok, so I'm gonna Random a Number from 1 to 9 you must to guess it\n"
-                          "you got 10 point and each wrong number your score will reduce by 1")
-                    input()
-                    clear()
-                    print("Ready?")
-                    sleep(1)
-                    clear()
-                    print("3")
-                    sleep(1)
-                    clear()
-                    print("2")
-                    sleep(1)
-                    clear()
-                    print("1")
-                    sleep(1)
-                    clear()
-
-                while True:
-
-                    new_in_selected_game = False
-
-                    if not menu:
-                        random_number = random.randint(1, 9)
-                        score = 10
-
-                        while True:
-
-                            while True:
-                                player_guess = input("Guess a number(1-9): ")
-                                try:
-                                    player_guess = int(player_guess)
-                                    break
-                                except ValueError:
-                                    clear()
-                                    print("Input a number Please")
-
-                            if player_guess < random_number:
-                                clear()
-                                print("Your Guess is too low.. ")
-                                score -= 1
-                            if player_guess > random_number:
-                                clear()
-                                print("Your Guess is too High.. ")
-                                score -= 1
-                            if player_guess == random_number:
-                                clear()
-                                print("You got it!")
-                                print(f"Your score : {score}")
-                                score = 10
-                                break
-
-                        while True:
-                            again = input("(y/n)do you wanna play again?\n> ")
-                            if again.lower().strip() == 'y' or again.lower().strip() == 'yes':
-                                menu = False
-                                clear()
-                                break
-                            if again.lower().strip() == 'n' or again.lower().strip() == 'no':
-                                menu = True
-                                del score, random_number, new_in_selected_game
-                                clear()
-                                print("Thanks for Playing\n")
-                                sleep(2)
-                                clear()
-                                break
-                            else:
-                                print("Wrong Input..")
-                                sleep(1)
-                                clear()
-                    else:
-                        break
-
-            else:
-                break
-
     def dad_jokes(self):
         clear()
         print("Dad Jokes 1.0")
@@ -106,7 +22,8 @@ class Game:
         new_in_selected_game = True
         while new_in_selected_game:
             while True:  # language selection menu loop
-                language = input("select a language or type 'exit' to back to menu :\n1.English\n2.Indonesia\n\n> ")
+                language = input(f"Welcome {self.name}\nselect a language or type 'exit' to back to menu "
+                                 f":\n1.English\n2.Indonesia\n\n> ")
                 clear()
                 if language.lower().strip() != "exit":
                     try:
@@ -151,7 +68,7 @@ class Game:
 
                 num_of_joke = 10
 
-                print("selamat datang, jokes bapak-bapak receh siap menghibur kamu ;)")
+                print(f"selamat datang {self.name}, jokes bapak-bapak receh siap menghibur kamu ;)")
                 sleep(3)
                 clear()
                 playing = True
@@ -272,14 +189,15 @@ class Game:
 
     def dragon_realm(self):
         def displayIntro():
-            print("You are in a land full of dragons. In front of you,you see two caves. In one cave, the dragon is\n "
-                  "friendly and will share his treasure with you. The other dragonis greedy and hungry, and will eat\n "
+            print("You are in a land full of dragons. In front of you,you see two caves. In one cave, the dragon is\n"
+                  "friendly and will share his treasure with you. The other dragon is greedy and hungry, and will "
+                  "eat\n"
                   "you on sight.\n")
 
         def chosen_Cave():
             cave = ''
             while cave != '1' and cave != '2':
-                print("which cave will you enter?")
+                print(f"which cave will you enter, {self.name} ?")
                 cave = input()
             return cave
 
@@ -316,14 +234,255 @@ class Game:
         import DaFluffyPotato
         DaFluffyPotato.run()
         del DaFluffyPotato
-        print(f"Welcome back {self.name}")
-        sleep(2)
         clear()
+        print(f"Welcome back {self.name}")
+        sleep(5)
+        clear()
+
+    def guessing_game(self):
+        sleep(1)
+        menu = False
+        new_in_selected_game = True
+
+        while True:
+            if not menu:
+                clear()
+                if new_in_selected_game:
+                    print(f"Welcome {self.name}..\nOk, so I'm gonna Random a Number from 1 to 9 you must to guess it\n"
+                          f"you got 10 point and each wrong number your score will reduce by 1")
+                    input()
+                    clear()
+                    print("Ready?")
+                    sleep(1)
+                    clear()
+                    print("3")
+                    sleep(1)
+                    clear()
+                    print("2")
+                    sleep(1)
+                    clear()
+                    print("1")
+                    sleep(1)
+                    clear()
+
+                while True:
+
+                    new_in_selected_game = False
+
+                    if not menu:
+                        random_number = random.randint(1, 9)
+                        score = 10
+
+                        while True:
+
+                            while True:
+                                player_guess = input("Guess a number(1-9): ")
+                                try:
+                                    player_guess = int(player_guess)
+                                    break
+                                except ValueError:
+                                    clear()
+                                    print("Input a number Please")
+
+                            if player_guess < random_number:
+                                clear()
+                                print("Your Guess is too low.. ")
+                                score -= 1
+                            if player_guess > random_number:
+                                clear()
+                                print("Your Guess is too High.. ")
+                                score -= 1
+                            if player_guess == random_number:
+                                clear()
+                                print("You got it!")
+                                print(f"Your score : {score}")
+                                score = 10
+                                break
+
+                        while True:
+                            again = input("(y/n)do you wanna play again?\n> ")
+                            if again.lower().strip() == 'y' or again.lower().strip() == 'yes':
+                                menu = False
+                                clear()
+                                break
+                            if again.lower().strip() == 'n' or again.lower().strip() == 'no':
+                                menu = True
+                                del score, random_number, new_in_selected_game
+                                clear()
+                                print("Thanks for Playing\n")
+                                sleep(2)
+                                clear()
+                                break
+                            else:
+                                print("Wrong Input..")
+                                sleep(1)
+                                clear()
+                    else:
+                        break
+
+            else:
+                break
+
+    def hang_man(self):
+        def get_secret_word(word_type):
+            animal = ["ant", "elephant", "fish", "frog", "pig", "cat", "dog", "lion"]
+            vehicle = ["car", "bus", "truck", 'airplane', 'ship', 'tractor']
+            thing = ['mouse', 'pencil', 'ruler', 'monitor', 'keyboard']
+            if word_type == "1":
+                world = animal
+                word_category = "Animal"
+            elif word_type == "2":
+                world = vehicle
+                word_category = "Vehicle"
+            elif word_type == "3":
+                world = thing
+                word_category = "Object Thing"
+            else:
+                world = animal + vehicle + thing
+                word_category = "Animal/Vehicle/Object"
+            word = random.choice(world)
+            return word, word_category
+
+        def display_board(missed_letter, correct_letter, secret_word):
+            print(HANGMAN_PICS[len(missed_letter)])
+            print()
+
+            print("Missed Letter :", end=" ")
+            for letter in missed_letter:
+                print(letter, end=" ")
+
+            print()
+
+            blanks = '_'*len(secret_word)
+            for i in range(len(secret_word)):
+                if secret_word[i] in correct_letter:
+                    blanks = blanks[:i] + secret_word[i] + blanks[i + 1:]
+            for letter in blanks:
+                print(letter, end=" ")
+
+        def get_guess(already_guessed):
+            while True:
+                print("Guess a letter :" )
+                guess = input("> ")
+                guess = guess.lower()
+                if len(guess) != 1:
+                    print("please insert only one number")
+                elif guess in already_guessed:
+                    print("you already guess that word, please guess another word")
+                elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+                    print("please insert only Letter")
+                else:
+                    return guess
+
+        def play_again():
+            print("do you want to play again ?")
+            return input().lower().startswith('y')
+        HANGMAN_PICS = ['''
+           +---+
+               |
+               |
+               |
+              ===''', '''
+           +---+
+           O   |
+               |
+               |
+              ===''', '''
+           +---+
+           O   |
+           |   |
+               |
+               ===''', '''
+           +---+
+           O   |
+          /|   |
+               |
+              ===''', '''
+           +---+
+           O   |
+          /|\\  |
+               |
+              ===''', '''
+           +---+
+           O   |
+          /|\\  |
+          /    |
+              ===''', '''
+           +---+
+           O   |
+          /|\\  |
+          / \\  |
+              ===''']
+        clear()
+        print(f"Welcome to Hangman Game, {self.name}..")
+        sleep(3)
+        clear()
+        missed_letter = ''
+        correct_letter = ''
+        game_is_done = False
+        while True:
+            choice = ["1", "2", "3", "4"]
+            print("Please Select Category first :\n1. Animal\n2. Vehicle \n3. Thing\n4. Anything")
+            category = input("> ")
+            if category in choice:
+                clear()
+                break
+            else:
+                print("Wrong Input")
+                sleep(1)
+                clear()
+        secret_word, category_name = get_secret_word(category)  # Generate the secret word
+        while True:
+            display_board(missed_letter, correct_letter, secret_word)
+
+            # let the player enter the letter
+            guess = get_guess(missed_letter + correct_letter)
+
+            if guess in secret_word:
+                correct_letter += guess
+
+                found_all_letter = True
+                for i in range(len(secret_word)):
+                    if secret_word[1] not in correct_letter:
+                        found_all_letter = False
+                        break
+                if found_all_letter:
+                    print("Yes! the secret word is '" + secret_word + "' You have won the game")
+                    game_is_done = True
+
+            else:
+                missed_letter += guess
+
+                if len(missed_letter) == len(HANGMAN_PICS)-1:
+                    display_board(missed_letter, correct_letter, secret_word)
+                    print("You have run out of guesses!\n After " + str(len(missed_letter)) + "missed guesses and " +
+                          str(len(correct_letter)) + "correct guesses, the word was :" + str(secret_word))
+                    game_is_done = True
+
+            if game_is_done:
+                if play_again():
+                    missed_letter = ''
+                    correct_letter = ''
+                    game_is_done = False
+                    while True:
+                        choice = ["1", "2", "3", "4"]
+                        print("Please Select Category first :\n1. Animal\n2. Vehicle \n3. Thing\n4. Anything")
+                        category = input("> ")
+                        if category in choice:
+                            clear()
+                            break
+                        else:
+                            print("Wrong Input")
+                            sleep(1)
+                            clear()
+                    secret_word, category_name = get_secret_word(category)  # Generate the secret word
+                else:
+                    break
 
 
 def game_menu():
     clear()
-    name = input("What is Your Name?\n>  ")
+    name = input("What is Your Name?\n> ")
     clear()
     age = input("How old are you?\n> ")
     clear()
@@ -334,11 +493,11 @@ def game_menu():
 
     while True:
         if new_game:
-            print(f"Hi, {name} Welcome to this game center")
+            print(f"Welcome to this game center, {name}\n")
 
         game_selection = input(f"Please Select a Game That You Wanna Play by calling the number or type 'exit' to "
-                               f"quit:\n1. Guess a number\n2. Dad Jokes\n3. Dragon Realm\n4. Gura Adventure  "
-                               f"\n5. Hangman")
+                               f"quit:\n\n1. Guess a number\n2. Dad Jokes\n3. Dragon Realm\n4. Gura Adventure  "
+                               f"\n5. Hangman\n\n> ")
         clear()
         game_selection = game_selection.lower()
 
@@ -370,7 +529,10 @@ def game_menu():
             wrong_counter = 0
             new_game = True
             print("coming soon..")
+            game = Game(name, age, gender)
+            game.hang_man()
             sleep(2)
+            clear()
             del game
 
         elif game_selection.lower().strip() == "exit":
